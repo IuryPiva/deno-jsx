a53c6032164eefcfb2057db26673d2044ca87049
Iury Piva <iurypiva@gmail.com>
2021-12-09 16:31:55 -0300

    working ssr and client

--- app.tsx
+++ app.tsx
@@ -1,25 +1,29 @@
 /** @jsx h */
-/// <reference lib="dom" />
-
-import { render, Component, Fragment, h } from "https://deno.land/x/nano_jsx@v0.0.26/mod.ts";
+import {
+  Component,
+  Fragment,
+  h,
+  render,
+} from "https://deno.land/x/nano_jsx@v0.0.26/mod.ts";
 
 export class App extends Component {
-  value = 0
+  value = 0;
 
   changeValue(newValue: number) {
-    this.value += newValue
-    this.update()
+    this.value += newValue;
+    this.update();
   }
 
   render() {
     return (
       <Fragment>
+        <h1>Hello nano!</h1>
         <div>Counter: {this.value}</div>
         <button onClick={() => this.changeValue(1)}>Increment</button>
         <button onClick={() => this.changeValue(-1)}>Decrement</button>
       </Fragment>
-    )
+    );
   }
 }
 
-render(<App />, document.body)
+render(<App />, document.body);
--- ssr.tsx
+++ ssr.tsx
@@ -1,33 +1,33 @@
 /** @jsx h */
-import {
-  h,
-  Helmet,
-  renderSSR,
-} from "https://deno.land/x/nano_jsx@v0.0.26/mod.ts";
-import { App } from "./app.tsx";
+/// <reference no-default-lib="true"/>
+/// <reference lib="dom" />
+/// <reference lib="deno.ns" />
+/// <reference lib="deno.unstable" />
 
-const { body, head, footer } = Helmet.SSR(renderSSR(<App />));
+import { h, renderSSR } from "https://deno.land/x/nano_jsx@v0.0.26/mod.ts";
 
-const html = `
-  <!DOCTYPE html>
-  <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Deno Nano JSX</title>
-    ${head.join("\n")}
-  </head>
-  <body>
-      ${body}
-      ${footer.join("\n")}
-      <script type="module" src="/bundle.js"></script>
-  </body>
-  </html>
-`;
-
-const path = "./index.html";
-await Deno.writeTextFile(path, html);
-const p = Deno.run({
-  cmd: ["deno", "fmt", path],
+const { files } = await Deno.emit(`./app.tsx`, {
+  bundle: "module",
+  compilerOptions: {
+    sourceMap: false,
+    lib: ["dom"]
+  },
+  check: false,
 });
+
+const html = renderSSR(() => (
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Nano JSX SSR</title>
+      <meta
+        name="description"
+        content="Server Side Rendered Nano JSX Application"
+      />
+    </head>
+    <body>
+      <script type="module">{files["deno:///bundle.js"]}</script>
+    </body>
+  </html>
+));
